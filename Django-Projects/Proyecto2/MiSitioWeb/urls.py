9d89106db65964059f1e0d627058d5427a92b98f
Matias <maticruz166@gmail.com>
2023-10-05 12:59:22 -0300

    Agreagados los ultimos trabajos

--- Django-Projects/Proyecto2/MiSitioWeb/urls.py
+++ Django-Projects/Proyecto2/MiSitioWeb/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MiPrimerProyecto project.
+URL configuration for MiSitioWeb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MiPrimerApp.urls')),
-    path('portfolio/', include('Portfolio.urls'))
+    path('blog/', include('Blog.urls'))
 ]
